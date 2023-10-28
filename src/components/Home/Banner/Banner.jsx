e9b6bf98fd683ddc5b46cd9bdbc9dcb65f542551
Rabby Khan <ajrabbyK72@gmail.com>
2023-10-28 10:42:01 +0600

    Created Banner Slider With SwiperJs

--- src/components/Home/Banner/Banner.jsx
+++ src/components/Home/Banner/Banner.jsx
@@ -1,4 +1,10 @@
-import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Pagination, Navigation, Autoplay } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+import "./Banner.css";
+
 import SlideOne from "./BannerSlides/SlideOne";
 import SLideTwo from "./BannerSlides/SLideTwo";
 import SlideThree from "./BannerSlides/SlideThree";
@@ -7,10 +13,32 @@ import SliderFour from "./BannerSlides/SliderFour";
 const Banner = () => {
   return (
     <>
-      <SliderFour />
-      <SlideThree />
-      <SLideTwo />
-      <SlideOne />
+      <Swiper
+        loop={true}
+        pagination={{
+          clickable: true,
+        }}
+        autoplay={{
+          delay: 7000,
+          disableOnInteraction: true,
+        }}
+        navigation={true}
+        modules={[Pagination, Navigation, Autoplay]}
+        id="banner__slider"
+      >
+        <SwiperSlide>
+          <SlideOne />
+        </SwiperSlide>
+        <SwiperSlide>
+          <SLideTwo />
+        </SwiperSlide>
+        <SwiperSlide>
+          <SlideThree />
+        </SwiperSlide>
+        <SwiperSlide>
+          <SliderFour />
+        </SwiperSlide>
+      </Swiper>
     </>
   );
 };
